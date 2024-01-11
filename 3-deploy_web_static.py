c24b69979676c45af25523fdcfbc67597fd3b490
AhmedSamy98 <ahmed1samy1998@gmail.com>
2024-01-11 15:03:32 -0800

    FIX

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ from fabric.operations import local, put, run
 from datetime import datetime as d
 from fabric.api import *
 
-env.hosts = ['34.74.120.150', '54.173.196.75']
+env.hosts = ['18.235.234.167', '54.236.43.210']
 created_archive = None
 
 
